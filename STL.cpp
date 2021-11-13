602b1226cecaa200d70583dceed0c8e343442f82
SlimShady666 <45724701+SlimShady666@users.noreply.github.com>
2021-11-13 22:12:24 +0800

    Update STL.cpp

--- STL.cpp
+++ STL.cpp
@@ -1,5 +1,10 @@
 #include<algorithm>
+#include<iostream>
+#include<set>
 using namespace std;
-void STL_algorithm(){
-  
+void STL_iterator(){
+  set<int>MySet;
+  set<int>::iterator it = MySet.begin();//得到指向第一个的指针
+  it++;//得到指向下一位的指针
+  set<int>::iterator it1 = MySet.begin()++;//错误，目前不知道为啥，但是报错了
 }
