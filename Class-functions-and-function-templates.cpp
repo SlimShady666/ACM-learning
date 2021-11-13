2e264803556fb6f5e0738535da6ec6c0d45d308f
SlimShady666 <45724701+SlimShady666@users.noreply.github.com>
2021-11-13 19:12:33 +0800

    Update Class-functions-and-function-templates.cpp

--- Class-functions-and-function-templates.cpp
+++ Class-functions-and-function-templates.cpp
@@ -3,11 +3,24 @@ using namespace std;
 
 //模板函数格式
 template<class T>
-int MyFunc(T a,T b){
+int MyAdd(T a,T b){
   return a + b;
 }
+//函数重载
+int MyAdd(int a,int b){
+  return a + b;
+}
+void test01(){
+  int a = 10;
+  int b = 20;
+  MyAdd(a,b);//使用时系统优先考虑普通函数
+  MyAdd<>(a,b);//强制调用模板函数
+}
 
-int main()
-{
+
+
+
+
+int main(){
   
 }
