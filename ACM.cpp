199a7220ffbdf9d8dc8a64f35f1ef74e937344a4
SlimShady666 <45724701+SlimShady666@users.noreply.github.com>
2022-07-17 17:12:37 +0800

    Create ACM.cpp

---
+++ ACM.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+//前缀和
+//用连续的和去求部分的值
+int sum[], dt[];
+int main()
+{
+  int i;
+  while(i--){
+    cin >> dt[i]
+    sum[i] += sum[i - 1];
+  }
+  dt[i] = sum[i] - sum[i - 1];
+}
